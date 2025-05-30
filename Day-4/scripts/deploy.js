f3b7993e649b3759a6c75ca615cf0c4296db3bb9
Arulselvan-65 <arulselvan2244@gmail.com>
2025-05-30 22:21:26 +0530

    Update Day-4 task

--- Day-4/scripts/deploy.js
+++ Day-4/scripts/deploy.js
@@ -1,7 +1,10 @@
 const {ethers} = require("hardhat")
 
 async function main() {
+    //Getting contract factory of the contract
     const conditionalStatementsContractFactory = await ethers.getContractFactory("ConditionalStatements");
+
+    //Deploying the contract factory
     const conditionalStatementsContract = await conditionalStatementsContractFactory.deploy();
 
     console.log("Conditional Statements Contract deployed to : ", conditionalStatementsContract.target);
